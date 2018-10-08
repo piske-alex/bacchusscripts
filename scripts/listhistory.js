950befe6b87489ef175392b9591e9c4269bc570b
alexho <Qwedf@bs.com>
2018-10-08 20:22:24 +0800

    new contract method

--- scripts/listhistory.js
+++ scripts/listhistory.js
@@ -272,8 +272,8 @@ let abi=[
         "type": "event"
     },
 ];
-var BACCContract = web3.eth.contract(abi);
-var myContract = BACCContract.at('0x0618822550a8483176e7b8cf7ce57cc26294a927');
+var myContract = new web3.eth.Contract(abi, '0x0618822550a8483176e7b8cf7ce57cc26294a927');
+
 myContract.getPastEvents('Transfer', {
     filter: {_from: '0xa558475680f0fafd6048b702b9334344236f86b4', _to: '0xa558475680f0fafd6048b702b9334344236f86b4'}, // Using an array means OR: e.g. 20 or 23
     fromBlock: 0,
