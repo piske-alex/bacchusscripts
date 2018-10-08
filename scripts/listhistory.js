c61606b8274b865f7681851b1842d44b36065168
alexho <Qwedf@bs.com>
2018-10-08 20:59:00 +0800

    ./

--- scripts/listhistory.js
+++ scripts/listhistory.js
@@ -4,7 +4,7 @@ const net = require('net');
 let abi = config.abi;
 const web3 = new Web3(new Web3.providers.IpcProvider('/home/alex/.ethereum/geth.ipc',net));
 console.log(`web3 version: ${web3.version}`)
-var myContract = new web3.eth.Contract(abi, '0x0618822550a8483176e7b8cf7ce57cc26294a927');
+var myContract = new web3.eth.Contract(abi, '0xB8c77482e45F1F44dE1745F52C74426C631bDD52');
 
 myContract.getPastEvents('Transfer', {
     //filter: {_from: '0xa558475680f0fafd6048b702b9334344236f86b4', _to: '0xa558475680f0fafd6048b702b9334344236f86b4'}, // Using an array means OR: e.g. 20 or 23
