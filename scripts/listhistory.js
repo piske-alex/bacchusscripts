154dd2c025e83df6c3a45dfd4dcff2cb74f0d09a
alexho <Qwedf@bs.com>
2018-10-08 20:57:09 +0800

    ./

--- scripts/listhistory.js
+++ scripts/listhistory.js
@@ -6,18 +6,18 @@ const web3 = new Web3(new Web3.providers.IpcProvider('/home/alex/.ethereum/geth.
 console.log(`web3 version: ${web3.version}`)
 var myContract = new web3.eth.Contract(abi, '0x0618822550a8483176e7b8cf7ce57cc26294a927');
 
-/*myContract.getPastEvents('Transfer', {
+myContract.getPastEvents('Transfer', {
     //filter: {_from: '0xa558475680f0fafd6048b702b9334344236f86b4', _to: '0xa558475680f0fafd6048b702b9334344236f86b4'}, // Using an array means OR: e.g. 20 or 23
     fromBlock: 6464629,
     toBlock: 'latest'
-}, function(error, events){ console.log(events); })
+}, function(error, events){ console.log(error.message+events); })
     .then(function(events){
         console.log(events) // same results as the optional callback above
-    });*/
+    });
 
-myContract.events.Transfer({}, { fromBlock: 6464623, toBlock: 'latest' }).get((error, eventResult) => {
+/*myContract.events.Transfer({}, { fromBlock: 6464623, toBlock: 'latest' }).get((error, eventResult) => {
     if (error)
         console.log('Error in myEvent event handler: ' + error);
     else
         console.log('myEvent: ' + JSON.stringify(eventResult.args));
-});
+});*/
