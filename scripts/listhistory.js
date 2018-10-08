234d1101fde05b1308582d60d2d2732347b0896f
alexho <Qwedf@bs.com>
2018-10-08 20:50:02 +0800

    ./

--- scripts/listhistory.js
+++ scripts/listhistory.js
@@ -7,9 +7,9 @@ const web3 = new Web3(new Web3.providers.IpcProvider('/home/alex/.ethereum/geth.
 var myContract = new web3.eth.Contract(abi, '0x0618822550a8483176e7b8cf7ce57cc26294a927');
 myContract.getPastEvents('allEvents', {
     //filter: {_from: '0xa558475680f0fafd6048b702b9334344236f86b4', _to: '0xa558475680f0fafd6048b702b9334344236f86b4'}, // Using an array means OR: e.g. 20 or 23
-    fromBlock: 6464629,
+    fromBlock: 6464000,
     toBlock: 'latest'
-}, function(error, events){ console.log(JSON.stringify(events)); })
+}, function(error, events){ //console.log(JSON.stringify(events)); })
     .then(function(events){
         console.log(JSON.stringify(events)) // same results as the optional callback above
     });
