126a5a5cb75c930aecc982f0141c7bfb65b22aaf
alexho <Qwedf@bs.com>
2018-10-19 15:11:11 +0800

    ./

--- scripts/listhistory.js
+++ scripts/listhistory.js
@@ -6,12 +6,12 @@ const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 //console.log(`web3 version: ${web3.version}`)
 var myContract = new web3.eth.Contract(abi, '0x0618822550a8483176e7b8cf7ce57cc26294a927');
 web3.eth.getBlockNumber(function(error, result){
-    console.log(result)
+    //console.log(result)
     myContract.getPastEvents('Transfer', {
         //filter: {_from: '0xa558475680f0fafd6048b702b9334344236f86b4', _to: '0xa558475680f0fafd6048b702b9334344236f86b4'}, // Using an array means OR: e.g. 20 or 23
-        fromBlock: 6481283,
+        fromBlock: result-5999,
         //fromBlock:result-999,
-        toBlock: 6481285
+        toBlock: 'latest'
     }, function(error, events){ console.log(error,events) })
         .then(function(events){
             events.forEach(function(element) {
