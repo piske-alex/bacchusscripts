797a9385f9070ebe9861675ba8297f3e3bee4df4
alexho <Qwedf@bs.com>
2018-10-19 14:46:05 +0800

    ./

--- scripts/listhistory.js
+++ scripts/listhistory.js
@@ -7,7 +7,7 @@ console.log(`web3 version: ${web3.version}`)
 var myContract = new web3.eth.Contract(abi, '0x0618822550a8483176e7b8cf7ce57cc26294a927');
 
 myContract.getPastEvents('Transfer', {
-    //filter: {_from: '0xa558475680f0fafd6048b702b9334344236f86b4', _to: '0xa558475680f0fafd6048b702b9334344236f86b4'}, // Using an array means OR: e.g. 20 or 23
+    filter: {_from: '0xa558475680f0fafd6048b702b9334344236f86b4', _to: '0xa558475680f0fafd6048b702b9334344236f86b4'}, // Using an array means OR: e.g. 20 or 23
     fromBlock: 6464629,
     toBlock: 'latest'
 }, function(error, events){  })
